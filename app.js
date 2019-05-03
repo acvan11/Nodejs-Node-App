fe64dc0d90fe81449e181304fa90926ee3d65d79
acvan11 <anvanc37@gmail.com>
2019-05-02 18:32:26 -0700

    argument parsing with yargs, using builder value

--- app.js
+++ app.js
@@ -9,8 +9,22 @@ yargs.version('1.1.0')
 yargs.command({
 	command: 'add',
 	describe: 'Add a new note',
-	handler: function () {
-		console.log('Adding a new note! ^^')
+	builder: {
+		title: {
+			describe: 'Note title',
+			demandOption: true,
+			type: 'string'
+		},
+		body: {
+			describe: 'full name of body',
+			demandOption: true,
+			type: 'string'
+		}
+	},
+
+	handler: function (argv) {
+		console.log('Actor: ', argv.actor)
+		console.log('title:', argv.title)
 	}
 })
 
@@ -40,4 +54,5 @@ yargs.command({
 	}
 })
 
-console.log(yargs.argv)
+yargs.parse()
+//console.log(yargs.argv)
